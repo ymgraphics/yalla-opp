95c0bf34f4ec25c43072878fc4a3a051506422df
Yasser Mokhtari <124399698+ymgraphics@users.noreply.github.com>
2024-11-28 17:31:29 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -9,7 +9,7 @@ def home():
     return "Bot is running!"
 
 def run():
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3500)))
 
 def keep_alive():
     t = Thread(target=run)
